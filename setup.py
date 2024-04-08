4169416ead4b33d37a2f7f719f5f2b959215edc2
Tom <37934171+ItsJustAce@users.noreply.github.com>
2024-04-08 17:43:46 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,17 @@
 from distutils.core import setup
 
-import os
+setup(name = "portablescrobbler",
+version = "0.1.1",
+description = "Command-line portable player log scrobbler",
+author = "Tom (ItsJustAce) <justace0@protonmail.me>",
+author_email = "justace0@protonmail.me",
+license = "gpl",
+url = "https://github.com/ItsJustAce/PortableScrobbler",
+scripts = ["portablescrobbler"],
+packages = ["st"],
+data_files = [
+    ("/usr/share/man/man1", ("scrobblethis.1.gz",)),
+    ]
+install_requires=["pylast"]
+)
 
-def get_build():
-    path = "./.build"
-    
-    if os.path.exists(path):
-        fp = open(path, "r")
-        build = eval(fp.read())
-        if os.path.exists("./.increase_build"):
-            build += 1
-        fp.close()
-    else:
-        build = 1
-    
-    fp = open(path, "w")
-    fp.write(str(build))
-    fp.close()
-    
-    return int(build)
-
-setup(
-    name = "portablescrobbler",
-    version = "0.1.%d" %get_build(),
-    description = "Command-line portable player log scrobbler",
-    author = "Tom (ItsJustAce) <justace0@protonmail.me>",
-    author_email = "justace0@protonmail.me",
-    license = "gpl",
-    url = "https://github.com/ItsJustAce/PortableScrobbler",
-    scripts = ["portablescrobbler"],
-    packages = ["st"],
-    data_files = [
-        ("/usr/share/man/man1", ("scrobblethis.1.gz",)),
-        ]
-    )
-    
